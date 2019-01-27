c302c667382a58a70b56c68d415003b2467b53cd
Harshitha Bobba <harshithabobba@pal-nat186-6-145.itap.purdue.edu>
2019-01-27 18:35:03 -0500

    Added a Tab Bar

--- Flixster/MovieDetailsViewController.swift
+++ Flixster/MovieDetailsViewController.swift
@@ -15,6 +15,7 @@ class MovieDetailsViewController: UIViewController {
     @IBOutlet weak var posterView: UIImageView!
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var synopsisLabel: UILabel!
+
     var movie: [String:Any]!
     override func viewDidLoad() {
         super.viewDidLoad()
---
+++ Flixster/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  Flixster
+//
+//  Created by Harshitha Bobba on 1/27/19.
+//  Copyright © 2019 Harshitha Bobba. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
